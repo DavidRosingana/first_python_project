c9a037a376312b82416b5584b82cb1e47cbc62a4
DavidRosingana <davidrosin21@gmail.com>
2019-03-13 09:52:39 +0100

    añadiendo combate pokemon por españa

--- Combate_pokemon.py
+++ Combate_pokemon.py
@@ -3,42 +3,41 @@ print("Puedes combatir contra Casado, Rivera o Santi")
 
 rival_elegido = input("Contra quien quieres combatir:").capitalize()
 vida_pikachu = 100
-vida_enemigo = 0
 
 if rival_elegido == "Casado":
     vida_enemigo = 100
+    nombre_rival = "Casado"
+    dano_enemigo = 12
 elif rival_elegido == "Rivera":
     vida_enemigo = 100
+    nombre_rival = "Rivera"
+    dano_enemigo = 8
 elif rival_elegido == "Santi":
     vida_enemigo = 100
+    nombre_rival = "Santi"
+    dano_enemigo = 9
 else:
     vida_enemigo = 1
-    print("has elegido al pokemon auxiliar Metapod")
+    nombre_rival = "Metapod"
+    dano_enemigo = 0
+    print("Has elegido al pokemon auxiliar Metapod")
 
 while vida_pikachu > 0 and vida_enemigo > 0:
-    ataque_pikachu = input("Que ataque quieres elegir:\n(Pikachu puede usar chispazo y bola voltio)").capitalize()
+    ataque_pikachu = input("Que ataque quieres elegir:\n(Pikachu puede usar chispazo y bola voltio) ").capitalize()
     if ataque_pikachu == "Chispazo":
+        dano_pikachu = 8
         print("Pikachu usó:", ataque_pikachu)
-        vida_enemigo -= 10
+        vida_enemigo -= dano_pikachu
 
-    if ataque_pikachu == "Bola voltio":
+    elif ataque_pikachu == "Bola voltio":
+        dano_pikachu = 12
         print("Pikachu usó:", ataque_pikachu)
-        vida_enemigo -= 12
+        vida_enemigo -= dano_pikachu
 
-    print("Vida del enemigo", vida_enemigo)
+    print("Vida de {} = {} ".format(nombre_rival, vida_enemigo))
 
-    if rival_elegido == "Casado":
-        print("Casado usó ataque aborto")
-        vida_pikachu -= 8
-
-    if rival_elegido == "Rivera":
-        print("Rivera usó ataque neoliberalismo")
-        vida_pikachu -= 1
-
-    if rival_elegido == "Santi":
-        print("Santi usó ataque ESPAÑA")
-        vida_pikachu -= 5
-    print("Vida de Pikachu:", vida_pikachu)
+    vida_pikachu -= dano_enemigo
+    print("Vida de Pikachu = ", vida_pikachu)
 
 if vida_enemigo <= 0:
     print("Pikachu ha roto ESPAÑA")
--- comer_helado.py
+++ comer_helado.py
@@ -1,8 +1,2 @@
 
-apetece_helado = input("te apetece un helado? (si/no)")
-tienes_dienro = input("tienes dinero? (si/no)")
-
-if apetece_helado == "si" and tienes_dinero == "si":
-    print("comete un helado")
-else:
-    print("pos que tonto")
+print(randint(1, 4))
