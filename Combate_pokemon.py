bf190625a237b8e045e38c823f8956af3b43df35
DavidRosingana <davidrosin21@gmail.com>
2019-03-14 19:58:03 +0100

    MEjorando el combate

--- Combate_pokemon.py
+++ Combate_pokemon.py
@@ -1,31 +1,38 @@
 print("Simulador de combates Pokemon por España.")
-elecc = input("Elige a tu Pokemon").capitalize()
+elecc = input("Elige a tu Pokemon ").capitalize()
 if elecc == "Pikachu":
-    print("Has elegido a Pikachu")
+    print("Has elegido a Pikachu ")
 else:
     print("No tenemos a ese Pokemon, se te asignará un Pikachu")
 
 print("Puedes combatir contra Casado, Rivera o Santiago")
 
-rival_elegido = input("Contra quién quieres combatir?").capitalize()
+rival_elegido = input("Contra quién quieres combatir? ").capitalize()
 vida_pikachu = 100
 
 if rival_elegido == "Casado":
     vida_enemigo = 100
     nombre_rival = "Casado"
     dano_enemigo = 12
+    nombre_ataque = "Titulo de master"
+
 elif rival_elegido == "Rivera":
     vida_enemigo = 100
     nombre_rival = "Rivera"
     dano_enemigo = 8
+    nombre_ataque = "Neoliberalismo"
+
 elif rival_elegido == "Santiago":
     vida_enemigo = 100
     nombre_rival = "Santiago"
     dano_enemigo = 9
+    nombre_ataque = "No somos fachas, pero..."
+
 else:
     vida_enemigo = 100000000000
     nombre_rival = "Trump"
     dano_enemigo = 99
+    nombre_ataque = "DESTRUCCIÓN INFINITA"
     print("Hala, por listo vas a combatir contra Trump")
 
 while vida_pikachu > 0 and vida_enemigo > 0:
@@ -47,10 +54,13 @@ while vida_pikachu > 0 and vida_enemigo > 0:
         print("Pikachu se hirió a si mismo")
         vida_pikachu -= 3
 
+    print("{} usó: {} ".format(nombre_rival, nombre_ataque))
     print("Vida de {} = {} ".format(nombre_rival, vida_enemigo))
 
     vida_pikachu -= dano_enemigo
     print("Vida de Pikachu = ", vida_pikachu)
+
+
 
 if vida_enemigo <= 0:
     print("Pikachu ha roto ESPAÑA")
